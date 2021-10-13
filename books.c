b03c0f3e2a24edb4ae04c04fd004d9f06a2cab6d
xpewa <xpewa10@gmail.com>
2021-10-13 10:35:36 +0300

    simple travis

--- books.c
+++ books.c
@@ -17,8 +17,6 @@ t_book* books_on_hands(t_library* library) {
         return NULL;
     }
     t_book* res = (t_book*)calloc(count_readers(library), sizeof(t_book));
-    if (res == NULL)
-        return ERROR_MEMORY;
     t_book* tmp = library->book_head;
     int i = 0;
     while (tmp != NULL) {
@@ -43,8 +41,9 @@ void push_book(t_library* library, t_book* book) {
         return;
     }
     t_book* tmp = library->book_head;
-    while (tmp->next != NULL)
+    while (tmp->next != NULL) {
         tmp = tmp->next;
+    }
     tmp->next = book;
 }
 
@@ -69,47 +68,57 @@ int static continue_scan() {
     char c = " ";
     printf("Do you want to add else one book? Enter y or n.\n");
     while (scanf("%c", &c) != 1 || c != 'y' && c != 'n');
-    if (c == 'y')
-        return SUCCESS;
-    else
-        return ERROR;
+    if (c == 'y') {
+      return SUCCESS;
+    }
+    return ERROR;
 }
 
 int static validate_isbn(const char* isbn) {
     for (int i = 0; i < 3; ++i) {
-        if (isbn[i]-'0' < 0 || isbn[i]-'0' > 9)
-            return ERROR;
+        if (isbn[i]-'0' < 0 || isbn[i]-'0' > 9) {
+          return ERROR;
+        }
     }
-    if (isbn[3] != '-' || isbn[5] != '-' || isbn[12] != '-' || isbn[15] != '-')
-        return ERROR;
-    if (isbn[4]-'0' < 0 || isbn[4]-'0' > 9)
-        return ERROR;
+    if (isbn[3] != '-' || isbn[5] != '-' || isbn[12] != '-' || isbn[15] != '-') {
+      return ERROR;
+    }
+    if (isbn[4]-'0' < 0 || isbn[4]-'0' > 9) {
+      return ERROR;
+    }
     for (int i = 6; i < 12; ++i) {
-        if (isbn[i]-'0' < 0 || isbn[i]-'0' > 9)
-            return ERROR;
+        if (isbn[i]-'0' < 0 || isbn[i]-'0' > 9) {
+          return ERROR;
+        }
     }
     for (int i = 13; i < 15; ++i) {
-        if (isbn[i]-'0' < 0 || isbn[i]-'0' > 9)
-            return ERROR;
+        if (isbn[i]-'0' < 0 || isbn[i]-'0' > 9) {
+          return ERROR;
+        }
     }
-    if (isbn[16]-'0' < 0 || isbn[16]-'0' > 9)
-        return ERROR;
-    if (isbn[17] != '\0')
-        return ERROR;
+    if (isbn[16]-'0' < 0 || isbn[16]-'0' > 9) {
+      return ERROR;
+    }
+    if (isbn[17] != '\0') {
+      return ERROR;
+    }
     return SUCCESS;
 }
 
 int validate_book(t_book* book) {
-    if (validate_isbn(book->ISBN) == ERROR || book->count_readers < 0 || book->count < 0)
-        return ERROR;
+    if (validate_isbn(book->ISBN) == ERROR || book->count_readers < 0 || book->count < 0) {
+      return ERROR;
+    }
     return SUCCESS;
 }
 
 int validate_reader(t_reader* reader) {
-    if (reader->date[0] < 1 || reader->date[0] > 31)
-        return ERROR;
-    if (reader->date[1] < 1 || reader->date[1] > 12)
-        return ERROR;
+    if (reader->date[0] < 1 || reader->date[0] > 31) {
+      return ERROR;
+    }
+    if (reader->date[1] < 1 || reader->date[1] > 12) {
+      return ERROR;
+    }
     return SUCCESS;
 }
 
@@ -134,11 +143,11 @@ int scan_books(t_library* library) {
         }
         if (book->count_readers == 0) {
             book->readers = NULL;
-        }
-        else {
+        } else {
             book->readers = (t_reader*)calloc(book->count_readers, sizeof(t_reader));
-            if (book->readers == NULL)
-                return ERROR_MEMORY;
+            if (book->readers == NULL) {
+              return ERROR_MEMORY;
+            }
         }
         for (int i = 0; i < book->count_readers; ++i) {
             printf("Surname\n"
@@ -159,8 +168,9 @@ int scan_books(t_library* library) {
 }
 
 void pop_book(t_library* library) {
-    if (library->book_head == NULL)
-        return;
+    if (library->book_head == NULL) {
+      return;
+    }
     t_book* tmp = library->book_head;
     library->book_head = library->book_head->next;
     free(tmp->readers);
@@ -168,7 +178,8 @@ void pop_book(t_library* library) {
 }
 
 void free_library(t_library* library) {
-    while (library->book_head != NULL)
-        pop_book(library);
+    while (library->book_head != NULL) {
+      pop_book(library);
+    }
     free(library);
 }
